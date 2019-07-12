f901ff0a4840cd166412d8bd3284476af1523ae0
VIINZL <v.heidenreich@icloud.com>
2019-07-12 05:02:50 +0200

    8ball commit test

---
+++ macie.py
@@ -0,0 +1,31 @@
+#lets hope this turns out great
+import discord
+from discord.ext import commands
+client = commands.Bot(command_prefix = '.')
+question = ('none')
+@client.event
+async def on_ready():
+    print('Bot now ready.')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms.')
+    print(f'{ctx.author} has used ping command.')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question = 'none'):
+    if question is 'none':
+        await ctx.send('Sorry you need to add a question for me to answer!')
+        pass
+    else:
+        responses = ['ja',
+                    'nö',
+                    'werweiß',
+                    'morgen evt...',
+                    'nattülliiiich']
+        answer = choice(responses)
+        await ctx.send(f'Question: {question}\nAnswer: {answer}')
+
+
+client.run('NTk5MDUxNzU1MTU3OTc5MTU2.XSfkcA.jA0MjlICbSvLJg5Bs1yNWtI1qZA')
